63617a9603cddd69e2dcb51b3a3e768701e3ba53
zengargoyle <zengargoyle@gmail.com>
2011-08-22 23:36:49 -0700

    automated install

--- init.sh
+++ init.sh
@@ -1,1 +1,13 @@
+
+OLD=$PWD
+cd $HOME
 ln -sf .vim/vimrc .vimrc
+cd .vim
+mkdir -p bundle
+for b in available-bundles/*; do
+	ln -sf "../$b" bundle
+done
+rm -f bundle/pathogen
+echo "Activated bundles:"
+ls bundle
+cd "$OLD"
