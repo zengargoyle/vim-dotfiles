7e2ba762401b5144eee7d026a4aa0ecb3012ffe1
Carl Hayter <hayter@usc.edu>
2011-11-19 22:37:33 -0800

    add submodule update

--- init.sh
+++ init.sh
@@ -4,6 +4,9 @@ cd $HOME
 ln -sf .vim/vimrc .vimrc
 cd .vim
 mkdir -p bundle
+if [ ! -d available-bundles/pathogen/.git ]; then
+    git submodule update --init
+fi
 for b in available-bundles/*; do
 	ln -sf "../$b" bundle
 done
